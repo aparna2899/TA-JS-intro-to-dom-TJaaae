509685218b89f981282b4968ef370c055ba34a9a
Aparna <tiwari.aparna2899@gmail.com>
2021-11-14 22:52:53 +0530

    Assignment: Manipulating Elements

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -5,13 +5,21 @@ default value to be "text" and return the input element inside label. (create it
 
 */
 
-// Your code goes here
+function createInputElm(label,type = 'text'){
+  let labelElm = document.createElement('label');
+  labelElm.innerText = label;
+  let inputElm = document.createElement('input');
+  inputElm.type = type;
+  labelElm.append(inputElm);
+  return labelElm;
+}
 
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
 
 // 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
+
 
 // Your code goes here
 
@@ -21,7 +29,17 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
-// Your code goes here
+
+
+function createList(array){
+  let ul = document.createElement('ul');
+  array.forEach(element => {
+    let li = document.createElement('li');
+    li.innerText = element;
+    ul.append(li);
+  });
+  return ul;
+}
 
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
@@ -29,7 +47,7 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 
 // 4. Create a function named `createTodoList` that accept and array of data like [{name: "Learn DOM", isDone: false}, {name: "Learn JS", isDone: true}] and returns
 // the html for single todo will look like given below
-/* 
+/*
 <ul>
   <li>
     <p>Learn DOM</p>
@@ -39,7 +57,26 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 </ul>
 */
 
-// Your code goes here
+function createTodoList(array){
+  let ul = document.createElement('ul');
+  array.forEach(element => {
+    let li = document.createElement('li');
+    ul.append(li);
+    let pElm = document.createElement('p');
+    pElm.innerText = element.name;
+    li.append(pElm);
+    let inputElm = document.createElement('input');
+    inputElm.checked = true
+    li.append(inputElm);
+    let spanElm = document.createElement('span');
+    if(element.isDone == false)
+    spanElm.innerText = '❌';
+    else
+    spanElm.innerText = '✅';
+    li.append(spanElm);
+  });
+  return ul;
+}
 
 // TEST
 createTodoList([
